823c9504c7fce51736a96ecfd59da4f008af819c
AliTaheri <alitaherimotlagh@yahoo.com>
2022-06-28 16:25:29 +0430

    schema change fully

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,50 +1,12 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Post,
-} from '@nestjs/common';
-import { Dokme } from '@prisma/client';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { DokmeService } from './dokme.service';
 
 @Controller('api')
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly dokmeService: DokmeService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @HttpCode(HttpStatus.OK)
-  @Get('dokme/list')
-  getAllDokme(): Promise<Dokme[]> {
-    return this.dokmeService.dokmes({ take: 10 });
-  }
-
-  @HttpCode(HttpStatus.OK)
-  @Post('dokme/create')
-  async createDokme(
-    @Body() postData: { title: string; url: string; description: string },
-  ): Promise<Dokme> {
-    const dto: Dokme = {
-      url: 'testUrl',
-      description: postData.description,
-      expireDate: new Date(),
-      isAnonymous: false,
-      link: postData.url,
-      title: postData.title,
-      siktirCount: 0,
-      visitCount: 0,
-      userId: 'b80057ec-1316-4937-8a67-62aa2bc669a1',
-      createdAt: new Date(),
-    };
-    return this.dokmeService.createDokme(dto);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DokmeService } from './dokme.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, DokmeService, PrismaService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
