fa3615693eb0ce8770e03ef0257b3fff14590a37
Ali Taheri <alitaherimotlagh@gmail.com>
2023-01-11 22:51:29 +0330

    go on render

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://siktir.fun',
-      'https://www.siktir.fun',
-      '*',
-    ],
-  });
+  app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // const httpAdapter = app.get(HttpAdapterHost);
