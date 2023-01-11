b4983ac84ab3c4d457380eb960c5e04a8d7f1c2c
Ali Taheri <alitaherimotlagh@gmail.com>
2023-01-11 22:05:04 +0330

    cors problem

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://siktir.fun',
+      'https://www.siktir.fun',
+      '*',
+    ],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // const httpAdapter = app.get(HttpAdapterHost);
