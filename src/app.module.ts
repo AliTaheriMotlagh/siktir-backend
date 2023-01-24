ff7ee9de75ecbf7e7f46a1edd0570061dc18e18e
Ali Taheri <alitaherimotlagh@gmail.com>
2023-01-24 21:14:55 +0330

    health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { SiktirModule } from './siktir/siktir.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { join } from 'path';
     AuthModule,
     DokmeModule,
     SiktirModule,
+    HealthModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/health/health.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheckService,
+  HttpHealthIndicator,
+  HealthCheck,
+} from '@nestjs/terminus';
+import { PrismaHealthIndicator } from 'src/health/prisma-health.indicator';
+
+@Controller('api/health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+    private db: PrismaHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('website', 'https://siktir.fun'),
+      () => this.db.isHealthy('database')
+    ]);
+  }
+}
