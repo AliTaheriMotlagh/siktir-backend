247714c4d9b93df1e36a15e26556b5c5a31a9109
Ali Taheri <alitaherimotlagh@gmail.com>
2023-01-24 13:43:52 +0330

    domain

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,16 @@ import { AuthModule } from './auth/auth.module';
 import { DokmeModule } from './dokme/dokme.module';
 import { ConfigModule } from '@nestjs/config';
 import { SiktirModule } from './siktir/siktir.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend-dist'),
+    }),
     AuthModule,
     DokmeModule,
     SiktirModule,
